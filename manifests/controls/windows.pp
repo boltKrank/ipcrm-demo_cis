725bfa915abb0e011367ee28969d11fbcc3a4d45
boltKrank <simon.anderson111@gmail.com>
2019-08-20 12:25:55 +1000

    Removed 2012 check

--- manifests/controls/windows.pp
+++ manifests/controls/windows.pp
@@ -1,6 +1,4 @@
 class demo_cis::controls::windows {
-  case $::operatingsystemmajrelease {
-    '2012 R2': {
       include ::demo_cis::controls::windows::server2012r2::cis_helpers
       include ::demo_cis::controls::windows::server2012r2::cis_1_1_1
       include ::demo_cis::controls::windows::server2012r2::cis_1_1_2
@@ -16,7 +14,4 @@ class demo_cis::controls::windows {
       include ::demo_cis::controls::windows::server2012r2::cis_17_3_1
       include ::demo_cis::controls::windows::server2012r2::cis_17_4_1
       include ::demo_cis::controls::windows::server2012r2::cis_17_5_1
-    }
-    default: {}
-  }
 }
